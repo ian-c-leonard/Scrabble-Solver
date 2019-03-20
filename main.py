5f1c8a4285a2722d30ae2af71cbe1c0cd6082d70
FermenFlo <joseph.b@husky.neu.edu>
2019-03-19 23:09:19 -0400

    added Agent class

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from collections import Counter
 
 class Scrabble():
     def __init__(self, size = 15, multipliers = None):
+        print('Initializing Scrabble...')
         assert size % 2, 'Board length must be odd.'
         self.agent = 0 # The agent's turn
         self.turn = 1 # What turn we're on
@@ -23,12 +24,26 @@ class Scrabble():
                        'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 
                        'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8, 
                        'Y': 4, 'Z': 10, 'BLANK': 0}
+        print('Optimizing Word Dictionary...')
         self.dawg = self._optimize_scrabble_words() # Optimize Scrabble words with a lookup dictionary
+        print('Initalizing Agents...')
+        self.agents = self._initialize_agents()
+        print('Done')
+        
+    
+    def _initialize_agents(self):
+        global agent_1
+        global agent_2
+        
+        agent_1 = Agent(self, 1)
+        agent_2 = Agent(self, 2)
+    
+        return [agent_1, agent_2]
+    
         
     def _optimize_scrabble_words(self):
         '''Initializes a Trie of all possible Scrabble words for optimized lookups.'''
         dawg = DAWG()
-
         dawg.add_all(WORDS)
     
         return dawg
@@ -48,6 +63,18 @@ class Scrabble():
         quadrant = quadrant + quadrant[len(quadrant)-2::-1]
         multipliers = [y + y[::-1][1:] for y in quadrant]
         return np.array(multipliers, dtype = object).reshape(self.size, self.size)
+    
+   
+
+    def is_over(self):
+        out_of_words = not self.tiles and any([not agent.tiles for agent in self.agents])
+        
+        out_of_possible_moves = any([agent.out_of_moves for agent in self.agents])
+        
+        if out_of_words or out_of_plays:
+            return True
+        
+        return False
     
             
         
@@ -72,106 +99,7 @@ class Scrabble():
             return board
 
     
-    def get_created_word_indices(self, word, indices):
-        '''Returns the indices of all newly createds from placing a word in a position'''
 
-        new_board = self.place(word, indices, mock = True)
-        horizontal = len(set([x[0] for x in indices])) == 1 # Word is being played horizontally
-        word_indices = []
-
-        if horizontal:
-            row = indices[0][0]
-
-            # Get all vertical words created
-            for _, j in indices:
-                col = [(x, j) for x in range(self.size)]
-                longest_indices = []
-                created = False
-
-                for index in col:
-                    if index in indices:  # The letter was just placed
-                        created = True
-
-                    if not new_board[index] and not created:
-                        longest_indices = []
-
-                    if new_board[index]:
-                        longest_indices.append(index)
-
-                    elif created:
-                        if len(longest_indices) > 1:
-                            word_indices.append(longest_indices)
-                        break
-
-
-            # Get all horizontal words created
-            curr_row = [(row, x) for x in range(self.size)]
-            longest_indices = []
-            created = False
-
-            for index in curr_row:
-                if index in indices:  # The letter was just placed
-                    created = True
-
-                if not new_board[index] and not created:
-                    longest_indices = []
-
-                if new_board[index]:
-                    longest_indices.append(index)
-
-                elif created:
-                    word_indices.append(longest_indices)
-                    break
-
-        else:
-            col = indices[0][1]
-
-            # Get all horizontal words created
-            for i, _ in indices:
-                row = [(i, x) for x in range(self.size)]
-                longest_indices = []
-                created = False
-
-                for index in row:
-                    if index in indices:  # The letter was just placed
-                        created = True
-
-                    if not new_board[index] and not created:
-                        longest_indices = []
-
-                    if new_board[index]:
-                        longest_indices.append(index)
-
-                    elif created:
-                        if len(longest_indices) > 1:
-                            word_indices.append(longest_indices)
-                        break
-
-
-            # Get the vertical word created
-            curr_col = [(x, col) for x in range(self.size)]
-            longest_indices = []
-            created = False
-
-            for index in curr_col:
-                if index in indices:  # The letter was just placed
-                    created = True
-
-                if not new_board[index] and not created:
-                    longest_indices = []
-
-                if new_board[index]:
-                    longest_indices.append(index)
-
-                elif created:
-                    word_indices.append(longest_indices)
-                    break
-
-        
-        word_indices = [indices for indices in word_indices if self.unplayed_indices(indices)]
-
-        print(word_indices)
-        return [{''.join([new_board[ind] for ind in indices]): indices} for indices in word_indices]
         
     
     def is_valid_placement(self, word, indices):
@@ -214,34 +142,7 @@ class Scrabble():
         
         
     
-    
-    def grid_optimizer(self, letters, grid, indices):
-        ## Rework grid_optimizer to take in a max_length grid (i.e. 12) and then find the largest word that CAN fit in said grid
-        ## i.e. [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '] --> CATS ( potentially)
 
-        # Deduce a list of words which fit in the given grid with letter constraints
-        search_string = '*' + ''.join([x if x else '?' for x in grid]).strip('?') + '*'
-
-        result = self.dawg.search(search_string)
-
-        counter = Counter(letters + [x for x in grid if x])
-
-        playable_words = [word for word in result if 
-                               all([counter[letter] >= sum([1 for x in word if x == letter]) for letter in word])]
-
-        max_word = (None, -np.inf)
-        for word in playable_words:
-
-            for start, index in enumerate(indices):
-                if all([grid[ind + start] == word[ind] for ind in range(len(word)) if grid[ind + start].isalnum()]):
-                    start_ind = start
-                    break
-
-            score = self.score(word, indices[start: start + len(word)])
-            if score > max_word[1]:
-                max_word = (word, score)
-            # Return max score of playable words with givin indices
-        return max_word[0]
 
 
     # Word, Indices --> Score
@@ -271,3 +172,187 @@ class Scrabble():
                 continue
             
         return sum(base_word_score) * word_multiplier
+    
+   
+
+class Agent():
+    def __init__(self, scrabble, number):
+        self.game = scrabble
+        self.number = number
+        self.tiles = []
+        self.opponent_tiles = []
+        self.out_of_moves = False
+        print(f"AGENT_{self.number}: Drawing Tiles And Guessing Opponent's Tiles")
+        self.draw()
+        self.guess_opponent_tiles()
+     
+    
+    def draw(self):
+        '''Draw from the global game's tile bag'''
+        n_missing = 7 - len(self.tiles)
+        drawn_tiles = list(np.random.choice(self.game.tiles, n_missing, replace = False))
+        
+        for x in drawn_tiles:
+            self.game.tiles.remove(x)
+        
+        self.tiles = drawn_tiles
+        
+    def guess_opponent_tiles(self):
+        """Guess opponent's tiles given the current distribution of tiles"""
+        self.opponent_tiles = np.random.choice(self.game.tiles, 7, replace = False)
+        
+    def move(self):
+        pass
+    
+    
+    def get_created_word_indices(self, word, indices):
+        '''Returns the indices of all newly createds from placing a word in a position'''
+
+        size = self.game.size
+        new_board = self.place(word, indices, mock = True)
+        horizontal = len(set([x[0] for x in indices])) == 1 # Word is being played horizontally
+        word_indices = []
+
+        if horizontal:
+            row = indices[0][0]
+
+            # Get all vertical words created
+            for _, j in indices:
+                col = [(x, j) for x in range(size)]
+                longest_indices = []
+                created = False
+
+                for index in col:
+                    if index in indices:  # The letter was just placed
+                        created = True
+
+                    if not new_board[index] and not created:
+                        longest_indices = []
+
+                    if new_board[index]:
+                        longest_indices.append(index)
+
+                    elif created:
+                        if len(longest_indices) > 1:
+                            word_indices.append(longest_indices)
+                        break
+
+
+            # Get all horizontal words created
+            curr_row = [(row, x) for x in range(size)]
+            longest_indices = []
+            created = False
+
+            for index in curr_row:
+                if index in indices:  # The letter was just placed
+                    created = True
+
+                if not new_board[index] and not created:
+                    longest_indices = []
+
+                if new_board[index]:
+                    longest_indices.append(index)
+
+                elif created:
+                    word_indices.append(longest_indices)
+                    break
+
+        else:
+            col = indices[0][1]
+
+            # Get all horizontal words created
+            for i, _ in indices:
+                row = [(i, x) for x in range(size)]
+                longest_indices = []
+                created = False
+
+                for index in row:
+                    if index in indices:  # The letter was just placed
+                        created = True
+
+                    if not new_board[index] and not created:
+                        longest_indices = []
+
+                    if new_board[index]:
+                        longest_indices.append(index)
+
+                    elif created:
+                        if len(longest_indices) > 1:
+                            word_indices.append(longest_indices)
+                        break
+
+
+            # Get the vertical word created
+            curr_col = [(x, col) for x in range(size)]
+            longest_indices = []
+            created = False
+
+            for index in curr_col:
+                if index in indices:  # The letter was just placed
+                    created = True
+
+                if not new_board[index] and not created:
+                    longest_indices = []
+
+                if new_board[index]:
+                    longest_indices.append(index)
+
+                elif created:
+                    word_indices.append(longest_indices)
+                    break
+
+        
+        word_indices = [indices for indices in word_indices if self.unplayed_indices(indices)]
+
+        print(word_indices)
+        return [{''.join([new_board[ind] for ind in indices]): indices} for indices in word_indices]
+    
+    def get_grids(self):
+        board = self.game.board.copy()
+        rows = [board[i] for i in range(self.game.size)]
+        cols = [board[:][i] for i in range(self.game.size)]
+        
+        return  rows + cols
+        
+    
+    def grid_optimizer(self, letters, grid, indices):
+        ## Rework grid_optimizer to take in a max_length grid (i.e. 12) and then find the largest word that CAN fit in said grid
+        ## i.e. [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '] --> CATS ( potentially)
+
+        # Deduce a list of words which fit in the given grid with letter constraints
+        search_string = '*' + ''.join([x if x else '?' for x in grid]).strip('?') + '*'
+
+        result = self.game.dawg.search(search_string)
+
+        counter = Counter(letters + [x for x in grid if x])
+
+        playable_words = [word for word in result if 
+                               all([counter[letter] >= sum([1 for x in word if x == letter]) for letter in word])]
+
+        max_word = (None, -np.inf)
+        for word in playable_words:
+
+            for start, index in enumerate(indices):
+                if all([grid[ind + start] == word[ind] for ind in range(len(word)) if grid[ind + start].isalnum()]):
+                    start_ind = start
+                    break
+
+            score = self.score(word, indices[start: start + len(word)])
+            if score > max_word[1]:
+                max_word = (word, score)
+                
+        # Return max score of playable words with givin indices
+        return max_word[0]
+    
+    
+    def place(self, word, indices, mock = False):
+        '''Place a word in a location on the board.
+           You can mock placements and return the would-be board state'''
+        
+        board = self.game.board.copy() if mock else self.game.board
+        
+        for count, ind in enumerate(indices):
+            board[ind] = word[count]
+        
+        if mock: # We want to actually return the board if it's a mock placemenent
+            return board
