1c9148ad95b2c0c65ebd6e8990ffdd97bae09f6d
Thomas Pedbereznak <tom@tomped.com>
2019-04-04 21:01:11 -0400

    moving constrains to Scrabble.py

--- src/Scrabble.py
+++ src/Scrabble.py
@@ -58,6 +58,36 @@ class Scrabble():
 
         return set.intersection(*sets)
 
+    def get_constraints(self, row):
+        ## Getting the start and end indices
+        start = [0]
+        end = []
+        for i in range(1, len(row) - 1):
+            letter = row[i]
+            if not letter and not row[i - 1]:
+                start.append(i)
+            if letter and not row[i - 1]:
+                start.append(i)
+            if letter and not row[i + 1]:
+                end.append(i)
+            if not letter and (((i - 1) in end) or (i - 1 == 0)) and not row[i + 1]:
+                end.append(i)
+        end.append(len(row) - 1)
+
+        ## Getting all possible pairs
+        pairs = [(starting_index, ending_index) for starting_index in start for ending_index in end if starting_index < ending_index]
+
+        def filter_unneeded_pairs(pair):
+            section = row[pair[0]: pair[1] + 1]
+            blank_count = section.count('')
+            return blank_count < len(section) and blank_count != 0
+
+        ## Filter out pairs that have all or no blanks
+        pairs = [pair for pair in pairs if filter_unneeded_pairs(pair)]
+
+        ## Making constraints
+        return [(pair[1] - pair[0] + 1, [(i - pair[0], row[i]) for i in range(pair[0], pair[1] + 1) if row[i]]) for pair in pairs]
+
     def _default_multipliers(self):
         quadrant = \
             [['3W', '', '', '2L', '', '', '', '3W'],
