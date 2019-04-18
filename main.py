8ea98ff3f4a948a9571bf55f8ee193327212f5b6
Thomas Pedbereznak <tom@tomped.com>
2019-04-18 13:15:38 -0400

    done a/b

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ from src.Agent import Agent
 from src.Minimax import Minimax
 import time
 
-PAS = 'pass'
-INF = float('inf')
-
 # Parsing command line arguments
 parser = argparse.ArgumentParser()
 
@@ -28,60 +25,13 @@ state.add_agent(0, agent_0)
 state.add_agent(1, agent_1)
 state.place('A', [(2, 2)], 0, rules)
 
-# state.draw(0)
-# state.draw(1)
-
-
-# print(state.board)
-
-# minimax_0 = Minimax(0, rules)
-
-# best_move = minimax_0.get_best_word(state, 0, 1)
-
-# print(best_move)
-
-# Setting up the game with two players
-
-# rules = ScrabbleRules(blanks = results.blanks, size = results.size)
-# state = GameState(blanks = results.blanks, size = results.size)
-
-# agent_0 = Agent()
-# agent_1 = Agent()
-# state.add_agent(0, agent_0)
-# state.add_agent(1, agent_1)
-
-# state.draw(0)
-# state.place('ARK', [(3, 2), (3, 3), (3, 4)], 0, rules)
-# state.draw(1)
-
-# move = state.get_legal_moves(1, rules)[10]
-# new_state = state.generate_successor(1, move[0], move[1], rules)
-
-# print(state.board)
-# print(state.agents[1].score)
-# print(new_state.board)
-# print(new_state.agents[1].score)
-# print(state.board)
-# print(state.agents[1].score)
-
-
-# new_state = state.generate_successor(0, 0, 0)
-
-# print('COPY')
-# copy_state = state.generate_successor(0, 1, 1)
-
-# print(copy_state.board)
-
-
-
-
 # Play
 agents = [0, 1]
 try:
     while True:
         for agent in agents:
             state.draw(agent)
-            best_move = Minimax(agent, rules).get_best_word(state, agent, 1, (-INF, PAS), (INF, PAS))
+            best_move = Minimax(agent, rules).get_best_word(state, agent, 1)
             print(f'Agent #{agent} played: {best_move[0]}')
             state.place(best_move[0], best_move[1], agent, rules)
             if state.is_over():
--- src/Minimax.py
+++ src/Minimax.py
@@ -16,24 +16,26 @@ class Minimax():
         return self.min_value(state, agent_id, depth, alpha, beta)[0]
 
     def max_value(self, state, agent_id, depth, alpha, beta):
-        v = (-INF, PAS)
+        max_eval = (-INF, PAS)
         for action in state.get_legal_moves(agent_id, self.rules):
-            v = max([v, (self.value(state.generate_successor(agent_id, action[0], action[1], self.rules), (agent_id + 1) % state.get_num_agents(), depth - 1, alpha, beta), action)], key=lambda pair: pair[0])
-            alpha = max([alpha, v], key=lambda pair: pair[0])
+            _eval = (self.value(state.generate_successor(agent_id, action[0], action[1], self.rules), (agent_id + 1) % state.get_num_agents(), depth - 1, alpha, beta), action)
+            max_eval = max([max_eval, _eval], key=lambda pair: pair[0])
+            alpha = max([alpha, _eval], key=lambda pair: pair[0])
             if beta[0] <= alpha[0]:
                 break
-        return v
+        return max_eval
 
     def min_value(self, state, agent_id, depth, alpha, beta):
-        v = (INF, PAS)
+        min_eval = (INF, PAS)
         for action in state.get_legal_moves(agent_id, self.rules):
-            v = min([v, (self.value(state.generate_successor(agent_id, action[0], action[1], self.rules), (agent_id + 1) % state.get_num_agents(), depth - 1, alpha, beta), action)], key=lambda pair: pair[0])
-            beta = min([beta, v], key=lambda pair: pair[0])
+            _eval = (self.value(state.generate_successor(agent_id, action[0], action[1], self.rules), (agent_id + 1) % state.get_num_agents(), depth - 1, alpha, beta), action)
+            min_eval = min([min_eval, _eval], key=lambda pair: pair[0])
+            beta = min([beta, _eval], key=lambda pair: pair[0])
             if beta[0] <= alpha[0]:
                 break
-        return v
+        return min_eval
 
-    def evaluation_function(self, state, agent_id): # TODO do it live (one liner)
+    def evaluation_function(self, state, agent_id):
         evauation = 0
         for _id in state.agents.keys():
             if _id == agent_id:
@@ -42,5 +44,5 @@ class Minimax():
                 evauation -= state.agents[_id].score
         return evauation
 
-    def get_best_word(self, state, agent_id, depth, alpha, beta):
-        return self.max_value(state, agent_id, depth * state.get_num_agents(), alpha, beta)[1] # TODO get depth value right
+    def get_best_word(self, state, agent_id, depth):
+        return self.max_value(state, agent_id, depth * state.get_num_agents(), (-INF, PAS), (INF, PAS))[1]
